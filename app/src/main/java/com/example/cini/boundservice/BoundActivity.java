642195967221cb05ec321085058460e736471535
logonrm <logonrm@fiap.com.br>
2017-06-12 23:04:42 -0300

    Initial commit

---
+++ app/src/main/java/com/example/cini/boundservice/BoundActivity.java
@@ -0,0 +1,14 @@
+package com.example.cini.boundservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by logonrm on 12/06/2017.
+ */
+
+
